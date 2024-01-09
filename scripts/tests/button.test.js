3f724d11df70ca7149a08cc9e3838950ff2ee44b
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-01-09 19:02:03 +0000

    Creating tests in JavaScript using Jest

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const buttonClick = require("../button");
+
+// beforeEach(() => {
+//     document.body.innerHTML = "<p id='par'></p>";
+// });
+
+// describe("DOM tests", () => {
+//     test("expects p content to change", () => {
+//         buttonClick();
+//         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+//     });
+// });
+
+beforeEach(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("DOM tests", () => {
+        test("expects p content to change", () => {
+            buttonClick();
+            expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+        });
+        test("h1 should exist", () => {
+            expect(document.getElementsByTagName("h1").length).toBe(1);
+        })
+    });
